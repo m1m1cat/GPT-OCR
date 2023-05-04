e3d91d5abd7fe56824bdd53f3f82581509af9626
cra5h <29671030+m1m1cat@users.noreply.github.com>
2023-05-04 12:58:06 +0800

    recognition result
    OCR识别结果查看，检查识别精准度
    View OCR recognition results and check recognition accuracy

---
+++ image.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -- coding utf-8 --
+# @AuthorCra5h
+# GPT-OCR-OCR result view
+
+import pytesseract
+from PIL import Image
+
+def test_ocr():
+    image_path = input("请输入图片路径：")
+    text = pytesseract.image_to_string(Image.open(image_path))
+    print(text)
+
+test_ocr()
+
